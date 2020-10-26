d5e21ef0d0239a64fe19e89959cd51e6ddc32a6a
haykazstepanyan <hstepanyan93@gmail.com>
2020-10-26 23:37:52 +0400

    primitives and enums

---
+++ enums.ts
@@ -0,0 +1,31 @@
+{
+//enums
+
+enum Numbers {
+  one,
+  two,
+  three
+}
+
+const num1 = Numbers.one;
+const num2 = Numbers.two;
+
+
+enum Fruits {
+  banana = 'banana',
+  apple = 'apple',
+  apricot = 'apricot'
+}
+
+const banana = Fruits.banana;
+
+
+enum DefaultNumbers {
+  item1 = 10,
+  item2,
+  item3 = 16,
+  item4
+}
+
+
+}
---
+++ primitives.ts
@@ -0,0 +1,29 @@
+{
+//string
+const str: string = 'Hello World'; 
+const str2: string =  `${str} from Typescript`;
+
+
+//number
+const num: number = 5;
+const num2: number = 1e4;
+
+
+//boolean
+let bool: boolean = true;
+bool = false;
+
+
+//bigInt
+// const bigInteger: bigint = 9876n;
+
+
+//array
+const numArray: number[] = [1, 3, 5, 7, 9];
+const strArray: string[] = ['Hello', 'World'];
+const genericType: Array<number> = [1, 2, 4, 8]; //array generic type
+
+//tuples
+const arr: [number, string] = [10, 'Hello'];
+arr[0] = 15;
+}
